60bd2f01feea742a22565ea844b2b53b3656a6e2
naman2807 <namanagarwaldbs438@gmail.com>
2021-10-14 23:43:00 +0530

    Implemented bottom navigation drawer and navigation drawer in AdminActivity.kt

--- app/src/main/java/com/example/academicapp/activities/AdminActivity.kt
+++ app/src/main/java/com/example/academicapp/activities/AdminActivity.kt
@@ -4,15 +4,20 @@ import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import com.example.academicapp.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
+
 
 class AdminActivity: AppCompatActivity() {
     private lateinit var toggle: ActionBarDrawerToggle
+    private lateinit var selectedFragment : Fragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,14 +25,14 @@ class AdminActivity: AppCompatActivity() {
 
         //Setting up Navigation Drawer
         val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
-        val navView : NavigationView = findViewById(R.id.navView)
+        val navView: NavigationView = findViewById(R.id.navView)
         toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         navView.setNavigationItemSelectedListener {
-            when(it.itemId){
+            when (it.itemId) {
                 R.id.setting -> Toast.makeText(this, "Navigation 1", Toast.LENGTH_SHORT).show()
                 R.id.gg -> Toast.makeText(this, "Navigation 2", Toast.LENGTH_SHORT).show()
                 R.id.setting2 -> Toast.makeText(this, "Navigation 3", Toast.LENGTH_SHORT).show()
@@ -35,5 +40,35 @@ class AdminActivity: AppCompatActivity() {
             return@setNavigationItemSelectedListener true
         }
 
+        //        Setting up Bottom Navigation Drawer
+        val bottomNav: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.bottom_home -> Toast.makeText(this, "Bottom Navigation 1", Toast.LENGTH_SHORT)
+                    .show()
+                R.id.bottom_1 -> Toast.makeText(this, "Bottom Navigation 2", Toast.LENGTH_SHORT)
+                    .show()
+                R.id.bottom_2e -> Toast.makeText(this, "Bottom Navigation 3", Toast.LENGTH_SHORT)
+                    .show()
+            }
+//            supportFragmentManager.beginTransaction().replace(R.id.nav_host_fragment, selectedFragment).commit()
+            return@setOnItemSelectedListener true
+
+
+        }
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finish()
+    }
+
 }
