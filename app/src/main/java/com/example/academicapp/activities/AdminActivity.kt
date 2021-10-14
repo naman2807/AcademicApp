6aaea81233176293df44141d25f8a1b691d6bc99
naman2807 <namanagarwaldbs438@gmail.com>
2021-10-14 23:27:55 +0530

    Implemented navigation bar

--- app/src/main/java/com/example/academicapp/activities/AdminActivity.kt
+++ app/src/main/java/com/example/academicapp/activities/AdminActivity.kt
@@ -1,13 +1,39 @@
 package com.example.academicapp.activities
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.academicapp.R
+import com.google.android.material.navigation.NavigationView
 
 class AdminActivity: AppCompatActivity() {
+    private lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin)
+
+        //Setting up Navigation Drawer
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
+        val navView : NavigationView = findViewById(R.id.navView)
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.setting -> Toast.makeText(this, "Navigation 1", Toast.LENGTH_SHORT).show()
+                R.id.gg -> Toast.makeText(this, "Navigation 2", Toast.LENGTH_SHORT).show()
+                R.id.setting2 -> Toast.makeText(this, "Navigation 3", Toast.LENGTH_SHORT).show()
+            }
+            return@setNavigationItemSelectedListener true
+        }
+
     }
 }
