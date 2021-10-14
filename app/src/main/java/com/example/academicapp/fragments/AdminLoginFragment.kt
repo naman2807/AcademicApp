675d27257987a205517a9983264775bba5698d3c
naman2807 <namanagarwaldbs438@gmail.com>
2021-10-14 17:11:22 +0530

    Overridden onViewCreated() method in AdminLoginFragment.kt

--- app/src/main/java/com/example/academicapp/fragments/AdminLoginFragment.kt
+++ app/src/main/java/com/example/academicapp/fragments/AdminLoginFragment.kt
@@ -1,10 +1,12 @@
 package com.example.academicapp.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.academicapp.activities.AdminActivity
 import com.example.academicapp.databinding.AdminLoginBinding
 
 class AdminLoginFragment: Fragment() {
@@ -17,5 +19,12 @@ class AdminLoginFragment: Fragment() {
     ): View? {
         binding = AdminLoginBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.login.setOnClickListener {
+            val intent = Intent(requireContext(), AdminActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
