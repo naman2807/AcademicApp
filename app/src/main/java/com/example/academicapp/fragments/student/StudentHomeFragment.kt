7acdbd436d78d41b315c6c958a25e8b4bf722e35
naman2807 <namanagarwaldbs438@gmail.com>
2021-10-14 22:00:49 +0530

    Implemented onCreateView in StudentHomeFragment.kt

--- app/src/main/java/com/example/academicapp/fragments/student/StudentHomeFragment.kt
+++ app/src/main/java/com/example/academicapp/fragments/student/StudentHomeFragment.kt
@@ -1,4 +1,21 @@
 package com.example.academicapp.fragments.student
 
-class StudentHomeFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.academicapp.databinding.StudentHomeFragmentBinding
+
+class StudentHomeFragment: Fragment() {
+    private lateinit var binding: StudentHomeFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = StudentHomeFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 }
