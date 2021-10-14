2ffffc808b7dfe10d83d7c84f9451d760223bee0
naman2807 <namanagarwaldbs438@gmail.com>
2021-10-14 17:22:57 +0530

    overridden onCreate() in StudentActivity.kt

--- app/src/main/java/com/example/academicapp/activities/StudentActivity.kt
+++ app/src/main/java/com/example/academicapp/activities/StudentActivity.kt
@@ -1,4 +1,12 @@
 package com.example.academicapp.activities
 
-class StudentActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.academicapp.R
+
+class StudentActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_student)
+    }
 }
