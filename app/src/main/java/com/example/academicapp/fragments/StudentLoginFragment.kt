7c896f861a742a0eee0c1ac7c8bf381e994a22cc
naman2807 <namanagarwaldbs438@gmail.com>
2021-10-14 16:54:14 +0530

    Overridden onCreateView() method in StudentLoginFragment.kt

--- app/src/main/java/com/example/academicapp/fragments/StudentLoginFragment.kt
+++ app/src/main/java/com/example/academicapp/fragments/StudentLoginFragment.kt
@@ -1,6 +1,21 @@
 package com.example.academicapp.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.academicapp.databinding.StudentLoginBinding
 
 class StudentLoginFragment: Fragment() {
+    private lateinit var binding: StudentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = StudentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 }
