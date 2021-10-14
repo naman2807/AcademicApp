cda082a7f1672d0684358f197532a5b8e34602e0
naman2807 <namanagarwaldbs438@gmail.com>
2021-10-14 17:21:04 +0530

    overridden onCreate() in AdminActivity.kt

--- app/src/main/java/com/example/academicapp/activities/AdminActivity.kt
+++ app/src/main/java/com/example/academicapp/activities/AdminActivity.kt
@@ -1,4 +1,13 @@
 package com.example.academicapp.activities
 
-class AdminActivity {
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.academicapp.R
+
+class AdminActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin)
+    }
 }
