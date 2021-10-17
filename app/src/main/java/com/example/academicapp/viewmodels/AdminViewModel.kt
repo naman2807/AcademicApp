6f7268634a370515217038200e5b81b3925e421f
naman2807 <namanagarwaldbs438@gmail.com>
2021-10-17 14:06:12 +0530

    Implemneted AdminViewModel.kt

--- app/src/main/java/com/example/academicapp/viewmodels/AdminViewModel.kt
+++ app/src/main/java/com/example/academicapp/viewmodels/AdminViewModel.kt
@@ -1,4 +1,20 @@
 package com.example.academicapp.viewmodels
 
-class AdminViewModel {
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.academicapp.fragments.admin.AdminHomeFragment
+
+class AdminViewModel: ViewModel() {
+    private var _selectedFragment = MutableLiveData<Fragment>()
+    val selectedFragment : LiveData<Fragment> get()  = _selectedFragment
+
+    init {
+        _selectedFragment.value = AdminHomeFragment()
+    }
+
+    fun selectFragment(fragment : Fragment){
+        _selectedFragment.value = fragment
+    }
 }
