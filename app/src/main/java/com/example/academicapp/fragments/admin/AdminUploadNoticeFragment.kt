d2642e7dd39883feb0693fffe06be65de9e77bda
naman2807 <namanagarwaldbs438@gmail.com>
2021-10-17 13:43:41 +0530

    Overridden onCreateView() in AdminUploadNoticeFragment.kt

--- app/src/main/java/com/example/academicapp/fragments/admin/AdminUploadNoticeFragment.kt
+++ app/src/main/java/com/example/academicapp/fragments/admin/AdminUploadNoticeFragment.kt
@@ -1,4 +1,21 @@
 package com.example.academicapp.fragments.admin
 
-class AdminUploadNoticeFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.academicapp.databinding.AdminUploadNoticeFragmentBinding
+
+class AdminUploadNoticeFragment: Fragment() {
+    private lateinit var binding: AdminUploadNoticeFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = AdminUploadNoticeFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 }
