93a1a9702b405fc949af2e0f5583f2cfd9440a98
naman2807 <namanagarwaldbs438@gmail.com>
2021-11-10 00:29:12 +0530

    Implemented function_text.xml

--- app/src/main/java/com/example/academicapp/fragments/admin/AdminUploadFunctionImageFragment.kt
+++ app/src/main/java/com/example/academicapp/fragments/admin/AdminUploadFunctionImageFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import com.example.academicapp.R
 import com.example.academicapp.databinding.AdminUploadFunctionImageFragmentBinding
 
 class AdminUploadFunctionImageFragment: Fragment() {
@@ -17,5 +19,10 @@ class AdminUploadFunctionImageFragment: Fragment() {
     ): View? {
         binding = AdminUploadFunctionImageFragmentBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val adapter = ArrayAdapter(requireContext(), R.layout.function_text, resources.getStringArray(R.array.functions))
     }
 }
