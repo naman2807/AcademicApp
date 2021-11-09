6a6c5d25ec758a0b678d29dd96c5d4eaafbd5f61
naman2807 <namanagarwaldbs438@gmail.com>
2021-11-09 21:56:41 +0530

    overridden onCreateView() method

--- app/src/main/java/com/example/academicapp/fragments/admin/AdminUploadFunctionImageFragment.kt
+++ app/src/main/java/com/example/academicapp/fragments/admin/AdminUploadFunctionImageFragment.kt
@@ -1,6 +1,21 @@
 package com.example.academicapp.fragments.admin
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.academicapp.databinding.AdminUploadFunctionImageFragmentBinding
 
 class AdminUploadFunctionImageFragment: Fragment() {
+    private lateinit var binding: AdminUploadFunctionImageFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = AdminUploadFunctionImageFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 }
