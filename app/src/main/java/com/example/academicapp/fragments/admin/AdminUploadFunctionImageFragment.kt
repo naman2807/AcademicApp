b36b8a12f55be0c98c57b31b37d58def27eb3693
naman2807 <namanagarwaldbs438@gmail.com>
2021-11-12 19:46:18 +0530

    Created uploadImage() method to upload image to firebase

--- app/src/main/java/com/example/academicapp/fragments/admin/AdminUploadFunctionImageFragment.kt
+++ app/src/main/java/com/example/academicapp/fragments/admin/AdminUploadFunctionImageFragment.kt
@@ -1,16 +1,31 @@
 package com.example.academicapp.fragments.admin
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.example.academicapp.R
 import com.example.academicapp.databinding.AdminUploadFunctionImageFragmentBinding
+import com.google.firebase.database.DatabaseReference
+import java.io.File
 
 class AdminUploadFunctionImageFragment: Fragment() {
     private lateinit var binding: AdminUploadFunctionImageFragmentBinding
+    private lateinit var imageUri: Uri
+    private lateinit var databaseReference: DatabaseReference
+
+    val getImage = registerForActivityResult(
+        ActivityResultContracts.GetContent(),
+        ActivityResultCallback {
+            imageUri = it
+            binding.functionImageEditText.setText(File(imageUri.path).name.toString())
+        }
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,6 +34,24 @@ class AdminUploadFunctionImageFragment: Fragment() {
     ): View? {
         binding = AdminUploadFunctionImageFragmentBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.functionImageEditText.setOnClickListener {
+            selectImage()
+        }
+
+        binding.uploadFunctionImageButton.setOnClickListener {
+            uploadImage()
+        }
+    }
+
+    private fun uploadImage() {
+
+    }
+
+    private fun selectImage() {
+        getImage.launch("image/*")
     }
 
     override fun onResume() {
