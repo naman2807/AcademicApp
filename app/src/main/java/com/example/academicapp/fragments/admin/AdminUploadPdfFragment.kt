90aa7529e55314622da5fa1e0819dab16765e455
naman2807 <namanagarwaldbs438@gmail.com>
2021-11-12 22:12:32 +0530

    Created private field of binding

--- app/src/main/java/com/example/academicapp/fragments/admin/AdminUploadPdfFragment.kt
+++ app/src/main/java/com/example/academicapp/fragments/admin/AdminUploadPdfFragment.kt
@@ -1,6 +1,20 @@
 package com.example.academicapp.fragments.admin
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.academicapp.databinding.AdminUploadPdfFragmentBinding
 
 class AdminUploadPdfFragment: Fragment() {
+    private lateinit var binding: AdminUploadPdfFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
 }
