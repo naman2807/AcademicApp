2b7e00cf26781228121e4ebac9776434ff090a50
naman2807 <namanagarwaldbs438@gmail.com>
2021-11-13 19:34:11 +0530

    overridden onCreateView() method

--- app/src/main/java/com/example/academicapp/fragments/admin/AdminAddFacultyFragment.kt
+++ app/src/main/java/com/example/academicapp/fragments/admin/AdminAddFacultyFragment.kt
@@ -1,9 +1,25 @@
 package com.example.academicapp.fragments.admin
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.academicapp.databinding.AdminAddFacultyFragmentBinding
 
 class AdminAddFacultyFragment: Fragment() {
     private lateinit var binding: AdminAddFacultyFragmentBinding
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = AdminAddFacultyFragmentBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+        return binding.root
+    }
 }
