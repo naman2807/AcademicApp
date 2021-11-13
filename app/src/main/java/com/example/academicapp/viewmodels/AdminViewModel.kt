cbb3f5a423eecb3c5c2b51c837bc4599a28b9495
naman2807 <namanagarwaldbs438@gmail.com>
2021-11-13 18:29:10 +0530

    Added setter methods for all uris

--- app/src/main/java/com/example/academicapp/viewmodels/AdminViewModel.kt
+++ app/src/main/java/com/example/academicapp/viewmodels/AdminViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.academicapp.viewmodels
 
+import android.net.Uri
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -8,7 +9,14 @@ import com.example.academicapp.fragments.admin.AdminHomeFragment
 
 class AdminViewModel: ViewModel() {
     private var _selectedFragment = MutableLiveData<Fragment>()
+    private var _noticeImageUri = MutableLiveData<Uri>()
+    private var _functionImageUri = MutableLiveData<Uri>()
+    private var _pdfUri = MutableLiveData<Uri>()
+
     val selectedFragment : LiveData<Fragment> get()  = _selectedFragment
+    val noticeImageUri : LiveData<Uri> get() = _noticeImageUri
+    val functionImageUri : LiveData<Uri> get() = _functionImageUri
+    val pdfUri : LiveData<Uri> get() = _pdfUri
 
     init {
         _selectedFragment.value = AdminHomeFragment()
@@ -16,5 +24,17 @@ class AdminViewModel: ViewModel() {
 
     fun selectFragment(fragment : Fragment){
         _selectedFragment.value = fragment
+    }
+
+    fun setNoticeImageUri(uri : Uri){
+        _noticeImageUri.value = uri
+    }
+
+    fun setFunctionImageUri(uri: Uri){
+        _functionImageUri.value = uri
+    }
+
+    fun setPdfUri(uri: Uri){
+        _pdfUri.value = uri
     }
 }
