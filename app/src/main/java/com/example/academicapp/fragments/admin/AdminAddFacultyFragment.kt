f625bd9c17ac5da1e56614b6cf828cbfd1ba2e52
naman2807 <namanagarwaldbs438@gmail.com>
2021-11-13 19:33:19 +0530

    Declared private binding variable

--- app/src/main/java/com/example/academicapp/fragments/admin/AdminAddFacultyFragment.kt
+++ app/src/main/java/com/example/academicapp/fragments/admin/AdminAddFacultyFragment.kt
@@ -1,6 +1,9 @@
 package com.example.academicapp.fragments.admin
 
 import androidx.fragment.app.Fragment
+import com.example.academicapp.databinding.AdminAddFacultyFragmentBinding
 
 class AdminAddFacultyFragment: Fragment() {
+    private lateinit var binding: AdminAddFacultyFragmentBinding
+
 }
